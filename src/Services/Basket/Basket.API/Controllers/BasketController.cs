484f2c696af4b77e6166d296fcd83e3964932006
hamidnch <hamidnch2007@gmail.com>
2021-09-30 14:31:02 +0330

    Basket microservices developed

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet(template: "{userName}", Name = "GetBasket")]
+        [ProducesResponseType(type: typeof(ShoppingCart), statusCode: StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasket(userName: userName);
+            return Ok(basket ?? new ShoppingCart(userName: userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(type: typeof(ShoppingCart), statusCode: StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasket(basket: basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.Delete(userName);
+            return Ok();
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -5,7 +5,7 @@
         public int Quantity { get; set; }
         public string Color { get; set; }
         public decimal Price { get; set; }
-        public int ProductId { get; set; }
+        public string ProductId { get; set; }
         public string ProductName { get; set; }
     }
 }
